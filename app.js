66402a3372aefe77d502b1d4da548d6ecc81e78d
vicceledon <vicceledon@gmail.com>
2022-03-12 12:27:49 -0300

    servidor con pacÃpeta pública

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ let express = require("express");
 let app = express();
 let path = require("path");
 
-app.listen(4000, () => console.log ("funciona"));
+app.use(express.static(path.join(path.resolve(__dirname), "./public")));
+
+app.listen(4000, () => console.log("funciona"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(path.resolve(__dirname), "./views/index.html"));
+});
