8c4eaea4acd1085ac669e3e1af388832c187d644
vicceledon <vicceledon@gmail.com>
2022-04-09 20:21:11 -0300

    menu hecho

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,10 @@
 const path = require("path");
+const { news, proms } = require("../../models/data");
 
 const homeController = {
   home: function (req, res) {
-    res.render("./main/index");
+    res.render("./main/index", { news, proms });
   },
 };
-
 
 module.exports = homeController;
