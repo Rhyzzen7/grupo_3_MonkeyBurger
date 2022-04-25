029161a45a7f10c5946326004eaa55046a2a19d2
vicceledon <vicceledon@gmail.com>
2022-04-25 20:09:01 -0300

    correccion de static public y method_override en app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = require("./routes/router");
+// Pasar poder usar los métodos PUT, PATCH y DELETE
+const methodOverride = require("method-override");
 
+//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.urlencoded({ extended: false }));
+// Pasar poder pisar el method="POST" en el formulario por PUT, PATCH y DELETE
+app.use(methodOverride("_method"));
+
+//Configuración del template engine
 app.set("view engine", "ejs");
-app.set ("views", path.join(__dirname, "./views"));
-
-app.use(express.static("public"));
+app.set("views", path.join(__dirname, "./views"));
 
 const PORT = process.env.PORT || 4000;
 
